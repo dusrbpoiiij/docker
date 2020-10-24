d83b65eff33eab680b43e608bea1a31929f690d5
yeonkyu <dusrbpoiiij@naver.com>
2020-10-25 01:02:10 +0900

    cached Layer

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,10 +3,24 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.json({
+    res.json([
+    {
         name: 'yeonkyu',
         email: 'yeonkyu@naver.com'
-    })   
+    },
+    {
+        name : 'Alice',
+        email : 'alice@gamail.com'
+    },
+    {
+        name : 'Jake',
+        email : 'jake@gamail.com'
+    },
+    {
+        name : 'Maria',
+        email : 'maria@gamail.com'
+    }
+])   
 });
 
 app.listen(port, () => console.log(`Example app listening op port ${port}`));
